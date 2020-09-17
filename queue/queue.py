df94774c6d4a1e16c1b82816bf5654445296f8b5
Janessa Garrow <janessa.garrow@gmail.com>
2020-09-16 20:11:03 -0600

    Complete step 1 for Queue

--- queue/queue.py
+++ queue/queue.py
@@ -13,16 +13,24 @@ return elements in First In First Out order.
 Stretch: What if you could only use instances of your Stack class to implement the Queue?
          What would that look like? How many Stacks would you need? Try it!
 """
+
+# Implement the Queue class using an array as the underlying storage structure.
+#    Make sure the Queue tests pass.
 class Queue:
     def __init__(self):
         self.size = 0
-        # self.storage = ?
+        self.storage = []
     
     def __len__(self):
-        pass
+        return self.size
 
     def enqueue(self, value):
-        pass
+        self.storage.append(value)
+        self.size = self.size + 1
+
 
     def dequeue(self):
-        pass
+        # if list is not empty
+        if self.size > 0:
+            self.size = self.size - 1
+            return self.storage.pop(0)
